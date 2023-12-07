54e5a7cd244384c44f9b3e05d84cd0f219ee2253
gabyfersan <44554967+gabyfersan@users.noreply.github.com>
2023-12-07 21:03:38 +0100

    Handle errors

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -3,7 +3,10 @@ import { z } from "zod";
 import prisma from "@/prisma/client";
 
 const createIssueSchema = z.object({
-  title: z.string().min(1).max(255),
+  title: z
+    .string()
+    .min(1, "Längden ska vara minst vara 1 tecken")
+    .max(255, "Längden ska vara minst vara 255 tecken"),
   description: z.string().min(1),
 });
 
@@ -13,7 +16,7 @@ export async function POST(request: NextRequest) {
 
   const validation = createIssueSchema.safeParse(body);
   if (!validation.success) {
-    return NextResponse.json(validation.error.errors, { status: 400 });
+    return NextResponse.json(validation.error.format(), { status: 400 });
   }
 
   const newIssue = await prisma.issue.create({
