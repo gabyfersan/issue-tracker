c8eb1f03969f38246218bec19ad2259f1b9e66d2
gabyfersan <44554967+gabyfersan@users.noreply.github.com>
2023-12-07 10:40:56 +0100

    Build the new issue page

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,10 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className} >
         <Theme>
           <NavBar />
-          {children}
+          <main className="p-5">{children}</main>
         </Theme>
       </body>
     </html>
