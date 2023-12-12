87497070e26ae649106884d8c96ebf4d1320278b
gabyfersan <44554967+gabyfersan@users.noreply.github.com>
2023-12-12 21:48:15 +0100

    Remove duplicate skeletons

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -14,7 +14,7 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
     try {
       setisDeleting(true);
       await axios.delete(`/api/issues/${issueId}`);
-      router.push("/issues");
+      router.push("/issues/list");
       router.refresh();
       setisDeleting(false);
     } catch (error) {
