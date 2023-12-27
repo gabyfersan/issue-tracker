96b4e22407c5ab883dca86a93fdca6343f95d87e
gabyfersan <44554967+gabyfersan@users.noreply.github.com>
2023-12-27 23:04:49 +0100

    Change to button and add dialog

--- app/sauna/page.tsx
+++ app/sauna/page.tsx
@@ -1,12 +1,21 @@
-import { Flex, Grid, Text } from "@radix-ui/themes";
+"use client";
+import { AlertDialog, Button, Flex, Grid, Text } from "@radix-ui/themes";
+import { useEffect, useState } from "react";
 
 const bookinHours = [];
 
 const getAllBookinHours = (index: number) => {
   const allBookinHours = [];
   console.log(getAll(index - 1));
-  for (let i = 1; i < 25; i++) {
-    allBookinHours.push(i === 1 ? { label: getAll(index - 1) } : {});
+  for (let i = 1; i <= 25; i++) {
+    allBookinHours.push(
+      i === 1
+        ? { label: getAll(index - 1).dateFormated }
+        : {
+            label: getAll(index - 1).dateNumerical + "T" + getAllHours()[i - 1],
+            //timeDate: getAll(index - 1).dateNumerical + getAllHours()[i - 1],
+          }
+    );
   }
   return allBookinHours;
 };
@@ -27,7 +36,7 @@ const getAll = (addNumbersOfDays: number) => {
   let date = new Date();
   date.setDate(date.getDate() + addNumbersOfDays);
 
-  return new Intl.DateTimeFormat("sv-SE", {
+  const dateFormated = new Intl.DateTimeFormat("sv-SE", {
     weekday: "long",
     year: "numeric",
     month: "short",
@@ -36,6 +45,15 @@ const getAll = (addNumbersOfDays: number) => {
     .format(date)
     .split(" ");
 
+  date.setDate(date.getDate());
+  //date.toISOString().slice(0, 11);
+  //const dateNumerical = date.toISOString().slice(0, 11);
+  const dateNumerical = new Intl.DateTimeFormat("sv-SE", {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  }).format(date);
+  return { dateFormated, dateNumerical };
   // const options = {
   //   weekday: "long",
   //   year: "numeric",
@@ -48,7 +66,7 @@ const getAll = (addNumbersOfDays: number) => {
 };
 
 const getAllHours = () => {
-  const hours = [];
+  const hours = [""];
   const currentDate = new Date();
   currentDate.setHours(0);
   currentDate.setMinutes(0);
@@ -60,88 +78,166 @@ const getAllHours = () => {
   return hours;
 };
 
-let div = {
-  height: "5em",
-  width: "5em",
-  border: "2px solid #ffa94d",
-  "border-radius": "5px",
+const moveToClosetsHour = (time: Date) => {
+  const newTime = new Date(time);
+
+  return newTime.setHours(newTime.getHours() + 1);
+};
+
+let base = {
+  //border: "2px solid #ffa94d",
+  //"border-radius": "5px",
+  //color: "#d9480f",
+  //"text-align": "center",
+  //"box-sizing": "border-box;",
+};
+
+let styleForTime = {
   "background-color": "#ffd8a8",
-  color: "#d9480f",
-  "text-align": "center",
+  ...base,
+  width: "4.5em",
+  height: "3em",
+  //"background-color": "#ffd8a8",
   margin: "2px 2px 2px 0",
   overflow: "clip",
 };
 
-let div2 = {
-  height: "5em",
-  width: "7em",
-  border: "2px solid #ffa94d",
-  "border-radius": "5px",
-  "background-color": "green",
-  color: "#d9480f",
-  "text-align": "center",
+let styleForBooking = {
+  ...base,
+  //width: "6em",
+  //height: "4.2em",
+  //"background-color": "green",
   margin: "2px",
   //"z-index": "-1",
 };
 
-let g = {
+let styleForGrid = {
   "grid-template-columns":
     "1fr 1fr 1fr 1fr  1fr  1fr  1fr 1fr 1fr  1fr  1fr  1fr",
   //  "position": "fixed"
   //contain: "paint",
   height: "600px",
   width: "100%",
-  "white-space": "nowrap",
   overflow: "auto",
 };
 
 const Sauna = () => {
+  const bookSauna = (e) => {
+    console.log(e.target);
+    console.log(e.target.dataset.dateAndTime);
+    setShowDialog(true);
+  };
+  const [showDialog, setShowDialog] = useState(false);
+  useEffect(() => {
+    document.getElementById("grid").style.height =
+      innerHeight -
+      document.getElementById("grid").getBoundingClientRect().top -
+      30 +
+      "px";
+  }, []);
   const allHours = getAllHours();
-  console.log();
+  console.log(allHours);
   return (
-    <Grid style={g}>
-      <Flex
-        direction='column'
-        // gap='0'
-        className=' sticky left-0 z-10 bg-white'
-      >
-        {allHours.map((a) => (
-          <div
-            key={a}
-            style={div}
-            className={`flex items-center justify-center sticky left-0`}
+    <>
+      {" "}
+      <AlertDialog.Root open={showDialog}>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Error</AlertDialog.Title>
+          <AlertDialog.Description>
+            This issue could not be deleted.
+          </AlertDialog.Description>
+          <Button
+            color='gray'
+            variant='soft'
+            mt='2'
+            onClick={() => setShowDialog(false)}
           >
-            <Text size='6'>{a}</Text>
-          </div>
-        ))}
-      </Flex>
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
-        <Flex direction='column' key={i}>
-          {getAllBookinHours(i).map((a, ii) => (
-            <div
-              key={Array.isArray(a.label) ? a.label[1] : a.label}
-              style={div2}
-              className={`flex items-center justify-center   	flex-col ${
-                a.label ? " !bg-white sticky top-0" : ""
-              }`}
-            >
-              {ii === 0 ? (
-                <>
+            OK
+          </Button>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+      <Grid style={styleForGrid} id='grid'>
+        <Flex
+          direction='column'
+          // gap='0'
+          className=' sticky left-0  bg-white'
+          style={{
+            zIndex: 10,
+          }}
+        >
+          {allHours.map((a, i) =>
+            i === 0 ? (
+              <div
+                style={{
+                  width: "4.5em",
+                  height: "3em",
+                  margin: "2px 2px 2px 0",
+                }}
+              ></div>
+            ) : (
+              <div
+                key={a}
+                style={{
+                  width: "4.5em",
+                  height: "3em",
+                  margin: "2px 2px 2px 0",
+                  borderRadius: "5px",
+                  backgroundColor: "#ffd8a8",
+                  overflow: "clip",
+                  zIndex: 99,
+                }}
+                className={`flex items-center justify-center sticky left-0 `}
+              >
+                <Text size='6'>{a}</Text>
+              </div>
+            )
+          )}
+        </Flex>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+          <Flex direction='column' key={i} onClick={bookSauna}>
+            {getAllBookinHours(i).map((a, ii) =>
+              ii === 0 ? (
+                <div
+                  key={Array.isArray(a.label) ? a.label[1] : a.label}
+                  style={{
+                    margin: "2px",
+                    borderRadius: "5px",
+                    backgroundColor: "#ffd8a8",
+                    opacity: 1,
+                    zIndex: 1,
+                  }}
+                  className={`flex items-center justify-center flex-col sticky top-0  `}
+                >
                   <Text size='3' className='block'>
                     {a?.label![0]}
                   </Text>
-
                   <Text size='3'> {a?.label![1] + " " + a?.label![2]}</Text>
-                </>
+                </div>
               ) : (
-                a.label
-              )}
-              <Text size='6'></Text>
-            </div>
-          ))}
-        </Flex>
-      ))}
-    </Grid>
+                <Button
+                  color='lime'
+                  variant='classic'
+                  size='4'
+                  disabled={new Date().getTime() > moveToClosetsHour(a.label)}
+                  data-date-and-time={a.label}
+                  key={a.label}
+                  style={{
+                    margin: "2px 2px 2px 2px",
+                    // zIndex: -2,
+                  }}
+                  className={`flex items-center justify-center flex-col hover:bg-purple-700
+            
+            ${new Date().getTime() > moveToClosetsHour(a.label) ? "" : ""}
+            `}
+                >
+                  <Text size='1'>{a.label}</Text>
+                </Button>
+              )
+            )}
+          </Flex>
+        ))}
+      </Grid>
+    </>
   );
 };
 
